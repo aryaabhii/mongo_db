a9967a5d5ee73cb19c157a676b7a9770da4b71a8
Abhijeet Kumar <aryaabhi84@gmail.com>
2023-09-16 12:40:28 +0530

    abhi_changes

--- mongoose/app.js
+++ mongoose/app.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 // connection create to db [STRATS]
 const dbUrl = "mongodb://127.0.0.1:27017/student";
+
 const connectionParams = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
